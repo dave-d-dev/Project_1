52d04e932658181f5bb05128f119d363ee28b6ad
dave-d-dev <dod.dobledd@gmail.com>
2025-07-11 04:37:58 +0100

    first commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'contactform_db');
+define('DB_USER', 'root'); // Change if necessary
+define('DB_PASS', ''); // Change if necessary
+
+?>
